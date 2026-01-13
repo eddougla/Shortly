0032cd86e1ce1dc2e96aafb49294d6a9135ca27a
Ezra Douglas <eddougla@hotmail.com>
2026-01-13 13:18:57 +0100

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl mb-4 font-sans">Welcome</h1>
+      <p className="font-cursive">Start building your Tailwind project</p>
+    </>
+  );
+}
+
+export default App;
