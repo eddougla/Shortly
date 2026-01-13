5423294dc594de096a963cbe18639489ba18b4bf
Ezra Douglas <eddougla@hotmail.com>
2026-01-13 18:07:10 +0100

    Validation

--- src/components/LinkShortnerForm.jsx
+++ src/components/LinkShortnerForm.jsx
@@ -1,16 +1,56 @@
+import { useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
 
 function LinkShortnerForm({ id }) {
+  const [error, setError] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  const validURL = (str) => {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" +
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+        "((\\d{1,3}\\.){3}\\d{1,3}))" +
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+        "(\\?[;&a-z\\d%_.~+=-]*)?" +
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    );
+    return !!pattern.test(str);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = e.target.elements["link-input"];
+    const value = input.value;
+
+    if (value === "") {
+      setError("Please enter something");
+      setHasError(true);
+      return;
+    } else if (!validURL(value)) {
+      setError("Please enter a valid URL");
+      setHasError(true);
+      return;
+    } else {
+      setError("");
+      setHasError(false);
+      alert("Success");
+    }
+  };
+
   return (
     <form
       id={id}
       className="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+      onSubmit={handleSubmit}
       noValidate
     >
       <Input
         type="url"
-        className="flex-1 p-3 border-2 rounded-lg placeholder-gray-400 focus:outline-none focus:border-cyan focus:ring-2 focus:ring-cyan transition-all"
+        className={`flex-1 p-3 border-2 rounded-lg placeholder-gray-400 focus:outline-none focus:border-cyan focus:ring-2 focus:ring-cyan transition-all ${
+          hasError ? "border-red" : ""
+        }`}
         placeholder="Shorten a link here"
         id="link-input"
         ariaLabel="Enter URL to shorten"
@@ -30,7 +70,9 @@ function LinkShortnerForm({ id }) {
         className="absolute left-7 bottom-3 text-red text-sm italic"
         role="alert"
         aria-live="polite"
-      ></div>
+      >
+        {error}
+      </div>
     </form>
   );
 }
